53e5dd3d95671aa2c45464f372b3b84b6a05de04
Cody Burrows <code1010@rochester.rr.com>
2015-01-29 20:27:35 -0500

    Revert "Tested, working"
    This reverts commit 1727eeb65749446def54808c3fef8b08de00a5bb.

--- src/org/usfirst/frc/team1559/robot/Robot.java
+++ src/org/usfirst/frc/team1559/robot/Robot.java
@@ -1,70 +1,23 @@
 
 package org.usfirst.frc.team1559.robot;
 
-import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType;
-import edu.wpi.first.wpilibj.Talon;
 
 //testing test test
 public class Robot extends IterativeRobot {
  
-	Joystick joy1;
-	Talon lf, lr, rf, rr;
-	RobotDrive rd;
-	Gyro g;
-	int count;
-	
     public void robotInit() {
-    	joy1 = new Joystick(0);
-    	lf = new Talon(9); //backwards
-    	lr = new Talon(6); //backwards
-    	rf = new Talon(8);
-    	rr = new Talon(7);
-    	rd = new RobotDrive(lf, lr, rf, rr);
-    	rd.setInvertedMotor(MotorType.kFrontLeft, true);
-    	rd.setInvertedMotor(MotorType.kRearLeft, true);
-    	rd.setMaxOutput(.5);
-    	g = new Gyro(1);
-    	count = 0;
+
     }
 
 
-    public void autonomousInit(){
-    	
-    	g.reset();
-    	
-    }
-    
     public void autonomousPeriodic() {
-    	
-    	if(count > 1000){
-    		rd.mecanumDrive_Cartesian(.5, 0, 0, g.getAngle());
-    	}
-    	    	
-    }
-    
-    public void teleopInit(){
-    	
-    	g.reset();
-    	
+
     }
 
 
     public void teleopPeriodic() {
         
-    	rd.mecanumDrive_Cartesian(joy1.getX(), joy1.getY(), joy1.getRawAxis(4), g.getAngle());
-//    	System.out.println(g.getAngle());
-    	
-    	if(joy1.getRawButton(1)){
-    		
-    		g.reset();
-    		
-    	}
-    	
-    	
     }
     
 
